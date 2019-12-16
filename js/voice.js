db8085f34664c588d2b6230a5c93d8cbf4e367bb
Janesh Sharma <janesh.sharma3@mail.dcu.ie>
2019-12-16 23:51:39 +0000

    fix some allignent issues

--- js/voice.js
+++ js/voice.js
@@ -2,12 +2,12 @@ let speechRec;
 let lang = navigator.language || "en-IE";   // use language set by users browser
 
 function listen() {
-  speechRec = new p5.SpeechRec(lang, gotSpeech);
-  speechRec.start();
+    speechRec = new p5.SpeechRec(lang, gotSpeech);
+    speechRec.start();
 }
 
 function gotSpeech() {
-  search(speechRec.resultString);
+    search(speechRec.resultString);     // search() is in main.js
 }
 
 $( "#voiceSearch" ).click(function() {
