95d07dcbe906e62417a6325f830951388ddd7ac5
Janesh Sharma <janesh.sharma3@mail.dcu.ie>
2019-12-17 14:05:36 +0000

    make movies clickable

--- js/movie.js
+++ js/movie.js
@@ -13,15 +13,58 @@ function get_movies(url) {
         let poster = "https://image.tmdb.org/t/p/w500/"
         $.each(movies, (index, movie) => {
             output += `
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 18.99rem;">
+            <a onclick="movie_onClick('${movie.id}')" class=btn btn-primary href="#">
             <img
               src="${poster}${movie.poster_path}"
               class="card-img-top" style="height: 25rem;
               alt="${movie.title} Movie Poster"
             />
+            </a>
           </div>`;
         });
 
+        $('#movies').html(output);
+    })
+
+    .catch((err) => {
+        console.log(err);
+    });
+}
+
+function movie_onClick(id) {
+    console.log(id);
+
+    axios.get("https://api.themoviedb.org/3/movie/"
+            + id
+            + "?api_key=49c34f6afc0f7fbb3c17e344b1abb335&language=en-US")
+    .then((response) => {
+        let movie = response.data
+        console.log(movie);
+        let poster = "https://image.tmdb.org/t/p/w500/";
+        let output = `
+        <div class="col-md-4">
+            <div class="card" style="width:25rem; height:40rem;">
+                <img
+                src="${poster}${movie.poster_path}"
+                class="card-img-top"
+                alt="${movie.title} Movie Poster"
+                />
+            </div>
+        </div>
+
+        <div class="col-md-8">
+            <h2>${movie.title}</h2>
+            <ul class="list-group">
+                <li class="list-group-item"><strong>Rating:</strong> ${movie.vote_average}</li>
+                <li class="list-group-item"><strong>Plot:</strong> ${movie.overview}</li>
+                <li class="list-group-item"><strong>Release Date:</strong> ${movie.release_date}</li>
+                <li class="list-group-item"><strong>Runtime:</strong> ${movie.runtime}</li>
+                <li class="list-group-item"><strong>Budget:</strong> ${movie.budget}</li>
+
+            </ul>
+        </div>
+        `;
         $('#movies').html(output);
     })
 
